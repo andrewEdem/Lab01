5ff7d27207418f97ca88a73c2247c0644eb56a66
Andrew Gbeddy <akg8@calvin.edu>
2016-09-23 15:35:20 -0400

    Initial commit

---
+++ MainActivity.java
@@ -0,0 +1,17 @@
+package edu.calvin.akg8.lab01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "akg8", Toast.LENGTH_LONG).show();
+    }
+}
